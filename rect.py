9a455685fb8aa8061400c0aeb70742edf0c5b44a
Matthias Bock <mail@matthiasbock.net>
2021-11-15 19:37:43 +0100

    Rewrote SVGRect class

--- rect.py
+++ rect.py
@@ -1,39 +1,14 @@
 #!/usr/bin/python
 #
-# Library to handle SVG rectangles
+# SVG rectangles
 #
 
-def xml_attribute(s, key):
-    p = s.find(key+'=\"')
-    q = f.find('"',p)
-    substring = s[p+len(key)+2:q]
-    if p > -1:
-        try:
-            val = float(substring)
-            return val
-        except:
-            return substring
-    return None
+from element import SVGElement
 
-class Rect:
-    #
-    # parse object from string
-    #
-    def __init__(this, s=None):
-        if s != None:
-            this.x = xml_attribute(s, 'x') or 0
-            this.y = xml_attribute(s, 'y') or 0
-            this.width = xml_attribute(s, "width") or 0
-            this.height = xml_attribute(s, "height") or 0
-            this.style = xml_attribute(s, "style") or ""
 
-    #
-    # export path as string
-    #
-    def __str__(this):
-        return '<rect x="' + str(this.x) \
-            + '" y="' + str(this.y) \
-            + '" width="' + str(this.width) \
-            + '" height="'+ str(this.height) \
-            + '" style="' + this.style \
-            + '" />'
+class SVGRect(SVGElement):
+    def __init__(self, attributes=[]):
+        self.x = attributes["x"] or 0.0
+        self.y = attributes["y"] or 0.0
+        self.width = attributes["width"] or 0.0
+        self.height = attributes["height"] or 0.0
