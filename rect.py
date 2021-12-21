a2373eb676929d852d62b4fd784ae4534e00a8b6
Matthias Bock <mail@matthiasbock.net>
2021-12-21 17:27:38 +0100

    Deriving SVGRect also from SVGBoundingBox

--- rect.py
+++ rect.py
@@ -4,11 +4,28 @@
 #
 
 from element import SVGElement
+from bbox import SVGBoundingBox
 
 
-class SVGRect(SVGElement):
+class SVGRect(SVGElement, SVGBoundingBox):
     def __init__(self, attributes=[]):
         self.x = attributes["x"] or 0.0
         self.y = attributes["y"] or 0.0
         self.width = attributes["width"] or 0.0
         self.height = attributes["height"] or 0.0
+
+        # Width and height shall not be negative
+        if self.width < 0.0:
+            self.x += self.width
+            self.width *= -1.0
+        if self.height < 0.0:
+            self.y += self.height
+            self.height *= -1.0
+
+        # Calculate bounding box in absolute coordinates
+        self.bottomLeftRelative = (self.x, self.y)
+        self.topRightRelative   = (self.x+self.width, self.y+self.height)
+        self.bottomLeftAbsolute = self.bottomLeftRelative * self.getCTM()
+        self.topRightAbsolute   = self.topRightRelative * self.getCTM()
+        (self.minX, self.minY) = self.bottomLeftAbsolute
+        (self.maxX, self.maxY) = self.topRightAbsolute
