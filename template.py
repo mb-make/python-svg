0149c6277d7ded1e6874fc10eae24808b2897a38
Matthias Bock <mail@matthiasbock.net>
2022-02-17 22:52:09 +0100

    SVGTemplate yields SVGTemplateMatch objects

--- template.py
+++ template.py
@@ -1,42 +1,83 @@
 #!/usr/bin/python3
 
-from svg import SVGParser
+from svg import SVGReader
+from rect import SVGRect
 from bbox import SVGBoundingBox
 
 
 #
-# Load a template SVG, extract a list of bounding boxes
-# and apply the latter for content extraction from a target SVG
+# Handle the results yielded from a selector match
 #
-class SVGTemplate(SVGParser):
-    def __init__(self, filename=None, bboxSelector="bbox[a-zA-Z0-9]", debug=False):
-        self.clear()
-        super().__init__(filename, debug)
+class SVGTemplateMatch:
+    def __init__(self, svg, template, selectorElement, elements=[]):
+        self.svg = svg
+        self.template = template
+        self.selectorElement = selectorElement
+        self.elements = elements
 
-    def clear(self):
-        super().clear()
-        self.boundingBoxes = []
+    def append(self, element):
+        self.elements.append(element)
+
+    def __len__(self):
+        return len(self.elements)
+
+    def getText(self):
+        # TODO: Extract text from elements
+        for e in results[label]:
+            if e.getTag().lower() == "text":
+                # TODO
+        return ""
+
+
+#
+# Use a template SVG to select elements from a target SVG
+#
+# Loads a template SVG, extract a list of selector elements
+# and allows to apply them to a target SVG.
+#
+class SVGTemplate(SVGReader):
+    def __init__(self, filename=None, debug=False):
+        SVGReader.__init__(self, filename, debug)
 
     #
-    # Iterate over all SVG elements and enumerate the ones
-    # where the id attribute matches the bbox selector
+    # Find all selector elements in the loaded SVG
     #
-    def parseBoundingBoxes(self):
-        self.boundingBoxes = []
-        # TODO: Enumerate bounding boxes
+    def getSelectorElements(self):
+        results = {}
+        key = "inkscape:label"
+        for e in self.getElementList():
+            if key in e.getAttributes().keys():
+                label = e.getAttributes(key)
+                tp = type(e)
+                if tp == SVGRect:
+                    results[label] = e
+                else:
+                    print("Warning: Skipping {:s}. Only rectangles are supported as selectors.".format(tp))
+        return results
 
     #
     # Apply this template to a target SVG and
-    # enumerate the content enclosed by bounding boxes
+    # return a dictionary of matched elements
     #
-    def apply(self, svgTarget=None):
-        if svgTarget is None:
-            print("Error: SVGTemplate.apply: Missing argument for svgTarget")
+    # @param targetSVG: The document to analyze as SVGReader or SVGElement object
+    #
+    def apply(self, targetSVG):
+        tp = type(targetSVG)
+        if (tp != SVGReader):
+        # if (tp != SVGElement) and (tp != SVGReader):
+            print("Error: Illegal argument type: {:s}".format(type(tp)))
             return None
 
         results = {}
-        for e in svgTarget.elementList:
-            for bbox in self.boundingBoxes:
+        selectors = self.getSelectorElements()
+        for q in selectors.keys():
+            bbox = selectors[q]
+            results[q] = SVGTemplateMatch(
+                            svg=targetSVG,
+                            template=self,
+                            selectorElement=bbox
+                            )
+            for e in targetSVG.getElementList():
                 if bbox.contains(e):
-                    if not (bbox in results.keys()):
-                        results[bbox]
+                    results[q].append(result)
+        return results
