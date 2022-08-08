c53fa02b0fab62ea26160953cc0be2cb3b9cd2dd
Matthias Bock <mail@matthiasbock.net>
2022-08-08 20:13:39 +0200

    Redirect all output to stdout

--- pytest.sh
+++ pytest.sh
@@ -5,7 +5,7 @@ test="pytest -vv --color=yes"
 $test
 
 while [ 1 ]; do
-  $test -r .
+  $test -r . 2>&1
   sleep 3
-  inotifywait $(find . -maxdepth 4 -type f) -e MODIFY
+  inotifywait $(find . -maxdepth 4 -type f) -e MODIFY 2>&1
 done
