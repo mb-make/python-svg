54a796a759c6f370d229eceeae4ba5053baf60e4
Matthias Bock <mail@matthiasbock.net>
2022-08-08 20:36:11 +0200

    Added proper template tests

--- templates/template_test.py
+++ templates/template_test.py
@@ -14,35 +14,44 @@ fn_target = os.path.join(here, "test_target.svg")
 fn_template = os.path.join(here, "test_template.svg")
 
 
-def test_template_loading():
-    svg = SVGTemplate(filename=fn_template, debug=True)
-    rects = svg.getElementsByTagName("rect")
-    paths = svg.getElementsByTagName("path")
-    assert(len(rects) == 4)
-    assert(len(paths) == 7)
-
-
 def test_target_loading():
     svg = SVGReader(filename=fn_target, debug=True)
     rects = svg.getElementsByTagName("rect")
     paths = svg.getElementsByTagName("path")
+    texts = svg.getElementsByTagName("text")
     assert(len(rects) == 0)
     assert(len(paths) == 8)
+    assert(len(texts) == 3)
+
+
+def test_template_loading():
+    svg = SVGTemplate(filename=fn_template, debug=True)
+    rects = svg.getElementsByTagName("rect")
+    paths = svg.getElementsByTagName("path")
+    texts = svg.getElementsByTagName("text")
+    assert(len(rects) == 4)
+    assert(len(paths) == 7)
+    assert(len(texts) == 0)
+
+    selectors = svg.getSelectorElements()
+    assert(len(selectors) == 4)
 
 
 def test_template_application():
     # Load template
     print("Loading template...")
     template = SVGTemplate(filename=fn_template, debug=False)
-    print(template)
+    selectors = template.getSelectorElements()
+    assert(len(selectors) == 4)
+    print("Got {:d} selectors".format(len(selectors)))
 
     # Load target
     print("Loading target SVG...")
     target = SVGReader(filename=fn_target, debug=False)
-    print(target)
 
     # Apply template
     print("Extracting content from target using template...")
     results = template.apply(target)
+    assert(results != None)
     print(results)
     # TODO: evaluate
