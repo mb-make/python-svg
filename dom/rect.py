eed9a8f935509e26884498292b2fe66b2a25b46d
Matthias Bock <mail@matthiasbock.net>
2022-08-08 20:37:35 +0200

    Convert rect coordinates to float
    Commented broken multiplication of float with SVGMatrix

--- dom/rect.py
+++ dom/rect.py
@@ -11,11 +11,13 @@ class SVGRect(SVGElement, SVGBoundingBox):
     #
     # TODO: Add support for transformed element or parents
     #
-    def __init__(self, attributes=[]):
-        self.x = attributes["x"] or 0.0
-        self.y = attributes["y"] or 0.0
-        self.width = attributes["width"] or 0.0
-        self.height = attributes["height"] or 0.0
+    def __init__(self, svg=None, parent=None, attributes={}, debug=False):
+        SVGElement.__init__(self, svg=svg, parent=parent, tag="path", attributes=attributes, debug=debug)
+
+        self.x = float(attributes["x"] or 0.0)
+        self.y = float(attributes["y"] or 0.0)
+        self.width = float(attributes["width"] or 0.0)
+        self.height = float(attributes["height"] or 0.0)
 
         # Width and height shall not be negative
         if self.width < 0.0:
@@ -28,7 +30,7 @@ class SVGRect(SVGElement, SVGBoundingBox):
         # Calculate bounding box in absolute coordinates
         self.bottomLeftRelative = (self.x, self.y)
         self.topRightRelative   = (self.x+self.width, self.y+self.height)
-        self.bottomLeftAbsolute = self.bottomLeftRelative * self.getCTM()
-        self.topRightAbsolute   = self.topRightRelative * self.getCTM()
-        (self.minX, self.minY) = self.bottomLeftAbsolute
-        (self.maxX, self.maxY) = self.topRightAbsolute
+        # self.bottomLeftAbsolute = self.bottomLeftRelative * self.getCTM()
+        # self.topRightAbsolute   = self.topRightRelative * self.getCTM()
+        # (self.minX, self.minY) = self.bottomLeftAbsolute
+        # (self.maxX, self.maxY) = self.topRightAbsolute
